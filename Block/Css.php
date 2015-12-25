55bcd83aed5b01f355b9b9d2565dd70621643feb
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-12-25 14:16:05 +0300

    no message

--- Block/Css.php
+++ Block/Css.php
@@ -1,6 +1,6 @@
 <?php
 namespace Dfe\ProductView\Block;
-use Df\Typography\Css as CssRenderer;
+use Df\Typography\Css as Renderer;
 use Dfe\ProductView\Settings\Price as SettingsPrice;
 use Dfe\ProductView\Settings\Sku as SettingsSku;
 use Dfe\ProductView\Settings\StockStatus as SettingsStockStatus;
@@ -14,41 +14,40 @@ class Css extends \Magento\Framework\View\Element\AbstractBlock {
 	 * @return string
 	 */
 	protected function _toHtml() {
-		/** @var string $result */
-		$result = '';
-		if (df_action_catalog_product_view()) {
-			/** @var string $cssS */
-			$cssS = df_concat_n(
+		return !df_action_catalog_product_view() ? '' : df_n_prepend(df_concat_n(
+			df_link_inline(SettingsSku::s()->fontL()->link())
+			, df_link_inline(SettingsSku::s()->fontV()->link())
+			, df_link_inline(SettingsStockStatus::s()->font()->link())
+			, df_link_inline(SettingsPrice::s()->font()->link())
+			, df_link_inline(SettingsTitle::s()->font()->link())
+			, df_style_inline(df_concat_n(
 				SettingsSku::s()->fontL()->css('.sku > .type')
 				,SettingsSku::s()->fontV()->css('.sku > .value')
 				,SettingsStockStatus::s()->font()->css('.stock')
 				,SettingsPrice::s()->font()->css('.product-info-main .price')
 				,SettingsTitle::s()->font()->css('.product-info-main [itemprop="name"]')
+				,$this->customCss()
+			))
+		));
+	}
+	
+	/**
+	 * 2015-12-25
+	 * @return string
+	 */
+	private function customCss() {
+		/** @var Renderer $renderer */
+		$renderer = Renderer::i();
+		/** @var string $labelSuffix */
+		$labelSuffix = SettingsSku::s()->labelSuffix();
+		if ('#:' !== $labelSuffix) {
+			$renderer->rule(
+				'content'
+				, df_quote_single($labelSuffix)
+				, '.product-info-main .product.attibute.sku .type:after'
 			);
-			if ($cssS) {
-				$result .= "\n" . df_concat_n(
-					df_link_inline(SettingsSku::s()->fontL()->link())
-					, df_link_inline(SettingsSku::s()->fontV()->link())
-					, df_link_inline(SettingsStockStatus::s()->font()->link())
-					, df_link_inline(SettingsPrice::s()->font()->link())
-					, df_link_inline(SettingsTitle::s()->font()->link())
-				);
-			}
-			/** @var CssRenderer $css */
-			$css = CssRenderer::i();
-			/** @var string $labelSuffix */
-			$labelSuffix = SettingsSku::s()->labelSuffix();
-			if ('#:' !== $labelSuffix) {
-				$css->rule(
-					'content'
-					, df_quote_single($labelSuffix)
-					, '.product-info-main .product.attibute.sku .type:after'
-				);
-			}
-			$cssS .= "\n" . $css->render();
-			$result .= "\n" . df_style_inline($cssS);
 		}
-		return $result;
+		return $renderer->render();
 	}
 }
 
