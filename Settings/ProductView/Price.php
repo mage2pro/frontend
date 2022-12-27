87f54eb5dba3aae34be02ee3a670735f01d31e3e
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-27 02:21:49 +0000

    https://github.com/mage2pro/core/issues/168

--- Settings/ProductView/Price.php
+++ Settings/ProductView/Price.php
@@ -4,7 +4,7 @@ use Df\Typography\Font;
 /** @method static Price s() */
 final class Price extends \Df\Config\Settings {
 	/** @return Font */
-	function font() {return $this->_font('font');}
+	function font():Font {return $this->_font('font');}
 	/**
 	 * @override
 	 * @see \Df\Config\Settings::prefix()
