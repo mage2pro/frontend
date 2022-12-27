a0d7de13640a4ed5cd90ea6526dc0bc4a716b2b9
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-27 18:59:52 +0000

    refactoring

--- Settings/ProductView/Wishlist.php
+++ Settings/ProductView/Wishlist.php
@@ -3,8 +3,8 @@ namespace Dfe\Frontend\Settings\ProductView;
 use Df\Typography\Font;
 /** @method static Wishlist s() */
 final class Wishlist extends \Df\Config\Settings {
-	/** @return Font */
-	function font() {return $this->_font('font');}
+	/** @used-by \Dfe\Frontend\Block\ProductView\Css::fonts() */
+	function font():Font {return $this->_font('font');}
 	/**
 	 * @override
 	 * @see \Df\Config\Settings::prefix()
