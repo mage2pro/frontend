1e7810126ef18f4bd099e1a5a7a1d02e71ddf46f
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-27 02:23:26 +0000

    https://github.com/mage2pro/core/issues/168

--- Settings/ProductView/ShortDescription.php
+++ Settings/ProductView/ShortDescription.php
@@ -4,7 +4,7 @@ use Df\Typography\Font;
 /** @method static ShortDescription s() */
 final class ShortDescription extends \Df\Config\Settings {
 	/** @return Font */
-	function font() {return $this->_font('font');}
+	function font():Font {return $this->_font('font');}
 	/**
 	 * @override
 	 * @see \Df\Config\Settings::prefix()
