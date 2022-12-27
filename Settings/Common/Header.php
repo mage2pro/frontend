30b75f118d9022cbafae907ee7090efd00c01e75
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-27 02:19:18 +0000

    refactoring

--- Settings/Common/Header.php
+++ Settings/Common/Header.php
@@ -4,6 +4,7 @@ namespace Dfe\Frontend\Settings\Common;
 final class Header extends \Df\Config\Settings {
 	/**
 	 * 2016-01-01 «Mage2.PRO» → «Frontend» → «Common» → «Header» → «Hide the Welcome Message?»
+	 * @used-by \Dfe\Frontend\Plugin\Theme\Block\Html\Header::aroundToHtml()
 	 */
 	function hideWelcome():bool {return $this->enable() && $this->b();}
 
