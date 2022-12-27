7ff0de66fa02ef60cb01e0f9a3ce955137877492
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-27 02:18:12 +0000

    refactoring

--- Plugin/Theme/Block/Html/Header.php
+++ Plugin/Theme/Block/Html/Header.php
@@ -11,10 +11,9 @@ class Header {
 	 * @see \Magento\Theme\Block\Html\Header::toHtml()
 	 * https://github.com/magento/magento2/blob/2.0.0/lib/internal/Magento/Framework/View/Element/AbstractBlock.php#L635-L662
 	 */
-	function aroundToHtml(Sb $sb, \Closure $f):string {
+	function aroundToHtml(Sb $sb, \Closure $f):string {/** @noinspection PhpUndefinedMethodInspection */ return
 		# 2016-01-01
 		# https://github.com/magento/magento2/blob/2.0.0/app/code/Magento/Theme/view/frontend/templates/html/header.phtml#L14-L15
-		/** @noinspection PhpUndefinedMethodInspection */
-		return 'welcome' === $sb->getShowPart() && S::s()->hideWelcome() ? '' : $f();
-	}
+		'welcome' === $sb->getShowPart() && S::s()->hideWelcome() ? '' : $f()
+	;}
 }
